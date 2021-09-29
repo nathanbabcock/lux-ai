49199421ad6b2e28370b3158b4438b2a61d1f1be
nbabcock <nathan.r.babcock@gmail.com>
2021-09-29 13:17:43 -0500

    Another stochastic tournament

--- scripts/generateStochasticAgents.ts
+++ scripts/generateStochasticAgents.ts
@@ -6,19 +6,10 @@ const sourceCode = readFileSync(resolve(__dirname, '../agents/stochastic-expande
 const ratios = [
   0,
   0.01,
+  0.02,
+  0.03,
+  0.04,
   0.05,
-  0.1,
-  0.2,
-  0.3,
-  0.4,
-  0.5,
-  0.6,
-  0.7,
-  0.8,
-  0.9,
-  0.95,
-  0.99,
-  1,
 ]
 
 ratios.forEach(ratio => {
