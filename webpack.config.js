3b86947829ac159f83f5f6a473743fc5fce00c2d
nbabcock <nathan.r.babcock@gmail.com>
2021-09-30 16:53:17 -0500

    Add webpack for Kaggle submission compilation

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'development',
+  entry: {
+    'tree-search': './agents/tree-search.ts',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'ts-loader',
+        }],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader",
+      },
+    ]
+  },
+  output: {
+    path: __dirname + '/dist/agents',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+}
