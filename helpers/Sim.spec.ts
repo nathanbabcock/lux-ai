3b97aa7bf54fdc67e3531da07db97bfa469fc7cf
nbabcock <nathan.r.babcock@gmail.com>
2021-10-05 16:15:35 -0500

    Test gather closest resource

--- helpers/Sim.spec.ts
+++ helpers/Sim.spec.ts
@@ -1,4 +1,4 @@
-import { GameMap, LuxDesignLogic, LuxMatchState } from '@lux-ai/2021-challenge'
+import { GameMap, LuxMatchState } from '@lux-ai/2021-challenge'
 import { Cell } from '../lux/Cell'
 import GAME_CONSTANTS from '../lux/game_constants.json'
 import { Position } from '../lux/Position'
@@ -8,7 +8,7 @@ import { initMatch } from './TreeSearch'
 import Turn from './Turn'
 
 describe('Sim', () => {
-  const init = async (replay?: string) => {
+  const initDebug = async (replay?: string) => {
     const match = await initMatch({
       storeReplay: !!replay,
       out: replay,
@@ -23,15 +23,31 @@ describe('Sim', () => {
     return { match, gameState, turn, game }
   }
 
+  const initSeed = async (replay?: string) => {
+    const match = await initMatch({
+      storeReplay: !!replay,
+      out: replay,
+      mapType: GameMap.Types.RANDOM,
+      width: 12,
+      height: 12,
+      seed: 123456789,
+    })
+    const game = (match.state as LuxMatchState).game
+    const gameState = Convert.toGameState(game, 0)
+    const turn = new Turn(gameState)
+
+    return { match, gameState, turn, game }
+  }
+
   test('Initializes a Turn object', async () => {
-    const { turn } = await init()
+    const { turn } = await initDebug()
 
     expect(turn.resourceTiles.length).toBeGreaterThan(0)
     expect(turn.resourceTiles[0]).toBeInstanceOf(Cell)
   })
 
   test('Resets gamestate with manual state setting', async () => {
-    const { match, turn } = await init()
+    const { match, turn } = await initDebug()
 
     const unit = turn.gameState.players[turn.gameState.id].units[0]
     unit.pos = new Position(5, 10)
@@ -53,7 +69,7 @@ describe('Sim', () => {
   })
 
   test('Move a unit in a direction', async () => {
-    const { match, turn } = await init('replays/test-move-in-direction.json')
+    const { match, turn } = await initDebug('replays/test-move-in-direction.json')
 
     const unit = turn.player.units[0]
     const oldPos = unit.pos
@@ -67,7 +83,7 @@ describe('Sim', () => {
   })
 
   test('Move to cell', async () => {
-    const { match, turn, game } = await init('replays/test-move-to-cell.json')
+    const { match, turn, game } = await initDebug('replays/test-move-to-cell.json')
 
     const dest = new Position(4, 1)
 
@@ -88,6 +104,49 @@ describe('Sim', () => {
     await update(5)
 
     expect(unit.pos.equals(dest)).toBe(true)
+
+    game.replay.writeOut(match.results)
+  })
+
+  test('Move to cell', async () => {
+    const { match, turn, game } = await initDebug('replays/test-move-to-cell.json')
+
+    const dest = new Position(4, 1)
+
+    const update = async (steps: number) => {
+      for (let i = 0; i < steps; i++) {
+        let unit = turn.player.units[0]
+        let action = turn.moveTo(unit, dest)
+        let game = await simulate(match, turn.gameState.id, action)
+        turn.update(game)
+      }
+    }
+
+    await update(5)
+
+    let unit = turn.player.units[0]
+    expect(unit.pos.equals(dest)).toBe(true)
+
+    await update(5)
+
+    expect(unit.pos.equals(dest)).toBe(true)
+
+    game.replay.writeOut(match.results)
+  })
+
+  test('Gather closest resource', async () => {
+    const { match, turn, game } = await initSeed('replays/test-gather-closest-resource.json')
+
+    for (let i = 0; i < 10; i++) {
+      let unit = turn.player.units[0]
+      let action = turn.gatherClosestResource(unit)
+      let game = await simulate(match, turn.gameState.id, action)
+      turn.update(game)
+    }
+
+    let unit = turn.player.units[0]
+    expect(unit.cargo.wood).toBe(100)
+    expect(unit.getCargoSpaceLeft()).toBe(0)
 
     game.replay.writeOut(match.results)
   })
