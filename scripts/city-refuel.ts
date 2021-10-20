f5f3541169ff42762f3e3d9a01749a2dd9e3225f
nbabcock <nathan.r.babcock@gmail.com>
2021-10-20 18:32:45 -0500

    Clean up notes on analysis scripts

--- scripts/city-refuel.ts
+++ scripts/city-refuel.ts
@@ -11,11 +11,14 @@ function refuel_city(side_length: number) {
   const inner_area = side_length * side_length - (perimeter + corners)
 
   const fuel_burn = (neighbors: number) => 23 - 5 * neighbors
-  const total_fuel_burn = corners * fuel_burn(2) + perimeter * fuel_burn(3) + inner_area * fuel_burn(4)
+  let total_fuel_burn = corners * fuel_burn(2) + perimeter * fuel_burn(3) + inner_area * fuel_burn(4)
+  if (side_length === 1) total_fuel_burn = fuel_burn(0)
+  else if (side_length === 0) total_fuel_burn = 0
   console.log(`Total fuel burn (per night turn) = ${total_fuel_burn}`)
 
   const nightly_cost = total_fuel_burn * night_length
   console.log(`Needs ${nightly_cost} to last all night`)
+  console.log(`vs. ${side_length*side_length*100} to rebuild from scratch`)
 
   const perTurn = nightly_cost / 40
   console.log(`Requires >= ${perTurn}, on average, per turn, day and night`)
@@ -23,3 +26,8 @@ function refuel_city(side_length: number) {
 
 for (let i = 1; i <= 10; i++)
   refuel_city(i)
+
+/**
+ * Notes:
+ * - Keeping a 3x3 city alive is more fuel-efficient than rebuilding it (not true for 1 or 2)
+ */
--- scripts/tree-regrowth.ts
+++ scripts/tree-regrowth.ts
@@ -29,9 +29,9 @@ function simulate_wood_collection_every_turn(collection_per_turn: number) {
 function simulate_wood_collection_periodic(collection_per_turn: number, on_turns: number, off_turns: number) {
   const wood_start = max_wood
 
-  // console.log('-----')
-  // console.log(`The tree starts with ${wood_start} wood`)
-  // console.log(`You collect ${collection_per_turn} wood per turn for ${on_turns} turns, and then wait ${off_turns} turns before repeating...`)
+  console.log('-----')
+  console.log(`The tree starts with ${wood_start} wood`)
+  console.log(`You collect ${collection_per_turn} wood per turn for ${on_turns} turns, and then wait ${off_turns} turns before repeating...`)
 
   const cycle_length = on_turns + off_turns
   let cur_wood = wood_start
@@ -48,12 +48,12 @@ function simulate_wood_collection_periodic(collection_per_turn: number, on_turns
     cur_wood *= wood_regrowth_rate
     cur_wood = Math.min(cur_wood, max_wood)
     if (cur_wood < 1) {
-      // console.log(`Wood ran out on turn ${i}`)
+      console.log(`Wood ran out on turn ${i}`)
       break
     }
   }
 
-  // console.log(`There will be ${cur_wood} left on turn ${i}`)
+  console.log(`There will be ${cur_wood} left on turn ${i}`)
   return {
     turn: i,
     wood: cur_wood,
@@ -62,25 +62,31 @@ function simulate_wood_collection_periodic(collection_per_turn: number, on_turns
 
 // for (let i = 0; i < 20; i++)
 //   simulate_wood_collection_every_turn(i)
-// Result: 12 wood every turn is sustainable (13 isn't)
 
 // simulate_wood_collection_periodic(20, 20, 10)
-// Result: 2 turns collecting 20 wood + 4 turns regrowing is 100% sustainable through turn 360
-// Same with 1 x 40, 4 x 0
-// Same with 4 x 20, 8 x 0
-// Same with 5 x 20, 10 x 0
-// Same with 10 x 20 (all night), 30 x 0 (all day)
 
-let min_ending_wood = max_wood
-for (let on_turns = 1; on_turns <= 40; on_turns++) {
-  for (let off_turns = 1; off_turns <= 40; off_turns++) {
-    const result = simulate_wood_collection_periodic(20, on_turns, off_turns)
-    if (result.turn < 330) continue
-    if (result.wood < min_ending_wood) {
-      min_ending_wood = result.wood
-      console.log(`New best: ${on_turns} on/${off_turns} off => ${Math.round(result.wood)} remaining on turn ${result.turn}`)
-    }
-  }
-}
-// New best: 24 on/32 off => 247 remaining on turn 361
-// New best: 25 on/32 off => 0 remaining on turn 342
+// let min_ending_wood = max_wood
+// for (let on_turns = 1; on_turns <= 40; on_turns++) {
+//   for (let off_turns = 1; off_turns <= 40; off_turns++) {
+//     const result = simulate_wood_collection_periodic(20, on_turns, off_turns)
+//     if (result.turn < 330) continue
+//     if (result.wood < min_ending_wood) {
+//       min_ending_wood = result.wood
+//       console.log(`New best: ${on_turns} on/${off_turns} off => ${Math.round(result.wood)} remaining on turn ${result.turn}`)
+//     }
+//   }
+// }
+
+simulate_wood_collection_periodic(20, 1, 1)
+
+/**
+ * Notes:
+ * - 12 wood every turn is sustainable (13 isn't)
+ * - 2 turns collecting 20 wood + 4 turns regrowing is 100% sustainable through turn 360
+ *   - Same with 1 x 40, 4 x 0
+ *   - Same with 4 x 20, 8 x 0
+ *   - Same with 5 x 20, 10 x 0
+ *   - Same with 10 x 20 (all night), 30 x 0 (all day)
+ * - 24 on/32 off => 247 remaining on turn 361
+ * - 25 on/32 off => 0 remaining on turn 342
+ */
