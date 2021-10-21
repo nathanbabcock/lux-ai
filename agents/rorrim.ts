485d3a75aedee5d7bb156fc1bab4e9f058de0c3e
nbabcock <nathan.r.babcock@gmail.com>
2021-10-21 15:05:44 -0500

    Mirror agent -- desync problems on unit death

---
+++ agents/rorrim.ts
@@ -0,0 +1,111 @@
+import { clearLog, log } from '../helpers/logging'
+import { Agent, annotate } from '../lux/Agent'
+import { Position } from '../lux/Position'
+import GAME_CONSTANTS from '../lux/game_constants.json'
+
+export type MirrorAxis = 'x' | 'y'
+
+let mirrorAxis: MirrorAxis
+const unitMirrors = new Map<string, string>()
+
+/** Assign a unit to a mirror based on where they were spawned */
+const birthAssignments: {
+  pos: Position,
+  mirror: string,
+}[] = []
+
+function getMirrorPos(pos: Position, mirrorAxis: MirrorAxis, mapSize: number): Position {
+  if (mirrorAxis === 'x') return new Position(pos.x, mapSize / 2 - (pos.y - mapSize / 2) - 1)
+  else return new Position(mapSize / 2 - (pos.x - mapSize / 2) - 1, pos.y)
+}
+
+const agent = new Agent()
+async function main() {
+  clearLog()
+  log('===============')
+  log('Mirror | rorriM')
+  log('===============')
+  log(new Date().toLocaleString())
+
+  agent.run(async gameState => {
+    log(`=== TURN ${gameState.turn}`)
+
+    const actions: string[] = []
+    const player = gameState.players[gameState.id]
+    const opponent = gameState.players[(gameState.id + 1) % 2]
+    const map = gameState.map
+
+    if (gameState.turn === 0) {
+      const citytile = Array.from(player.cities.values())[0].citytiles[0]
+      const otherCitytile = Array.from(opponent.cities.values())[0].citytiles[0]
+      if (citytile.pos.x === otherCitytile.pos.x)
+        mirrorAxis = 'x'
+      else
+        mirrorAxis = 'y'
+
+      unitMirrors.set(player.units[0].id, opponent.units[0].id)
+
+      log(`Identified mirror axis = ${mirrorAxis}`)
+    }
+
+    for (const unit of player.units) {
+      let mirror = unitMirrors.get(unit.id)
+
+      if (!mirror) {
+        const birthAssignment = birthAssignments.find(a => a.pos.equals(unit.pos))
+        if (!birthAssignment) continue
+        birthAssignments.splice(birthAssignments.indexOf(birthAssignment), 1)
+        unitMirrors.set(unit.id, birthAssignment.mirror)
+        mirror = unitMirrors.get(unit.id)
+      }
+
+      const mirrorUnit = opponent.units.find(u => u.id === mirror)
+      if (!mirrorUnit) {
+        actions.push(annotate.sidetext(`Mirror unit for ${unit.id} not found`))
+        log(`Mirror unit for ${unit.id} not found`)
+        continue
+      } else {
+        actions.push(annotate.line(unit.pos.x, unit.pos.y,mirrorUnit.pos.x, mirrorUnit.pos.y))
+      }
+
+      // If there's a city under an opposing mirrored unit, build one
+      const cell = map.getCellByPos(unit.pos)
+      const mirrorCell = map.getCellByPos(mirrorUnit.pos)
+      if (unit.pos.equals(getMirrorPos(mirrorCell.pos, mirrorAxis, map.width))  && mirrorCell.citytile && !cell.citytile && unit.canAct()) {
+        actions.push(unit.buildCity())
+        continue
+      }
+      
+      // Mimic movements of mirrored units
+      const mirrorPos = getMirrorPos(mirrorUnit.pos, mirrorAxis, map.width)
+      if (!unit.pos.equals(mirrorPos) && unit.canAct()) {
+        actions.push(unit.move(unit.pos.directionTo(mirrorPos)))
+        continue
+      }
+    }
+
+    const citytiles = Array.from(player.cities.values()).flatMap(c => c.citytiles)
+    for (const citytile of citytiles) {
+      if (!citytile.canAct()) continue
+
+      const mirrorPos = getMirrorPos(citytile.pos, mirrorAxis, map.width)
+      const mirrorCell = map.getCellByPos(mirrorPos)
+      if (!mirrorCell.citytile) continue
+
+      const newUnit = opponent.units.find(unit => unit.pos.equals(mirrorPos) && !Array.from(unitMirrors.values()).find(id => id === unit.id))
+      if (!newUnit) {
+        actions.push(citytile.research())
+        continue
+      }
+
+      if (newUnit.type === GAME_CONSTANTS.UNIT_TYPES.WORKER)
+        actions.push(citytile.buildWorker())
+      else 
+        actions.push(citytile.buildCart())
+      birthAssignments.push({ pos: citytile.pos, mirror: newUnit.id })
+    }
+    return actions
+  })
+}
+
+main()
