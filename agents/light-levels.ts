06ba841af061c8e22b4698c9760123f69df5bae9
nbabcock <nathan.r.babcock@gmail.com>
2021-10-26 14:54:02 -0500

    Light-seeking agent

--- agents/light-levels.ts
+++ agents/light-levels.ts
@@ -1,9 +1,12 @@
-import { getResources } from '../helpers/helpers'
+import { getNeighbors, getResources } from '../helpers/helpers'
 import { clearLog, log } from '../helpers/logging'
 import GAME_CONSTANTS from '../lux/game_constants.json'
 import { Agent, annotate } from '../lux/Agent'
 import { Cell } from '../lux/Cell'
 import { writeFileSync } from 'fs'
+import { Player } from '../lux/Player'
+import Turn from '../helpers/Turn'
+import { randomActions } from './random'
 
 const agent = new Agent()
 
@@ -16,7 +19,7 @@ async function main() {
 
   agent.run(async gameState => {
     const actions = []
-    if (gameState.turn  > 0) return actions
+    // if (gameState.turn  > 0) return actions
 
     // Let's imagine every entity in the game emits light (it's thematic, after all)
     // The brightness of the light is proportional to the fuel value of the corresponding resource
@@ -25,6 +28,7 @@ async function main() {
     // Possibly, shadows could be cast by cities and other obstacles
 
     const map = gameState.map
+    const player = gameState.players[gameState.id]
     const lightLevels = new Map<Cell, number>()
     const resources = getResources(map)
 
@@ -37,8 +41,15 @@ async function main() {
 
     for (const resourceCell of resources) {
       const brightness: number = GAME_CONSTANTS.PARAMETERS.RESOURCE_TO_FUEL_RATE[resourceCell.resource.type.toUpperCase()]
-      const luminosity = brightness * resourceCell.resource.amount
-      // if (resourceCell.resource.type !== 'wood') continue
+      let researchFactor = 1
+      if (resourceCell.resource.type === 'coal')
+        researchFactor = player.researchPoints / GAME_CONSTANTS.PARAMETERS.RESEARCH_REQUIREMENTS.COAL
+      else if (resourceCell.resource.type === 'uranium')
+        researchFactor = player.researchPoints / GAME_CONSTANTS.PARAMETERS.RESEARCH_REQUIREMENTS.URANIUM
+      researchFactor = Math.min(researchFactor, 1)
+      // if (gameState.turn === 0)
+      //   log(`researchFactor is ${researchFactor}`)
+      const luminosity = brightness * resourceCell.resource.amount * researchFactor
 
       for (let y = 0; y < map.height; y++) {
         for (let x = 0; x < map.width; x++) {
@@ -57,7 +68,6 @@ async function main() {
       if (value > maxLightLevel) maxLightLevel = value
     })
 
-
     const levelsArray: number[][] = []
     for (let y = 0; y < map.height; y++) {
       levelsArray.push([])
@@ -65,12 +75,60 @@ async function main() {
         const cell = map.getCell(x, y)
         const lightLevel = lightLevels.get(cell)
         const normalizedLightLevel = Math.round((lightLevel / maxLightLevel) * 255)
-        actions.push(annotate.text(cell.pos.x, cell.pos.y, `${normalizedLightLevel}`))
+        if (gameState.turn === 0)
+          actions.push(annotate.text(cell.pos.x, cell.pos.y, `${normalizedLightLevel}`))
         levelsArray[y].push(normalizedLightLevel)
       }
     }
 
-    writeFileSync('../../replays/light-levels.json', JSON.stringify(levelsArray))
+    if (gameState.turn === 0)
+      writeFileSync('../../replays/light-levels.json', JSON.stringify(levelsArray))
+
+    for (const unit of player.units) {
+      const cell = map.getCellByPos(unit.pos)
+      const adjacent = getNeighbors(cell, map);
+      ([ cell, ...adjacent ]).forEach(cell => {
+        actions.push(annotate.text(cell.pos.x, cell.pos.y, `${levelsArray[cell.pos.y][cell.pos.x]}`))
+      })
+
+      if (!unit.canAct()) continue
+
+      // go towards the light
+      let maxLight = 0
+      let maxLightCell: Cell = null
+      let minLight = Infinity
+      let minLightCell: Cell = null
+      for (const neighbor of adjacent) {
+        const lightLevel = lightLevels.get(neighbor)
+        if (lightLevel > maxLight) {
+          maxLight = lightLevel
+          maxLightCell = neighbor
+        }
+        if (lightLevel < maxLight) {
+          minLight = lightLevel
+          minLightCell = neighbor
+        }
+      }
+      if (unit.canAct() && unit.getCargoSpaceLeft() === 0 && unit.canBuild(map)) {
+        actions.push(unit.buildCity())
+        continue
+      }
+      if (unit.canAct() && unit.getCargoSpaceLeft() === 0) {
+        if (!minLightCell) continue
+
+        const dir = unit.pos.directionTo(minLightCell.pos)
+        actions.push(unit.move(dir))
+        continue
+      }
+      if (!maxLightCell) continue
+      if (maxLightCell.citytile) continue
+
+      const dir = unit.pos.directionTo(maxLightCell.pos)
+      actions.push(unit.move(dir))
+    }
+
+    const turn = new Turn(gameState)
+    actions.push(turn.autoCities())
 
     return actions
   })
