47fd685ea69273ec46bb0f9bc619a2808799f1d4
nbabcock <nathan.r.babcock@gmail.com>
2021-10-27 21:36:57 -0500

    Import Munkres (Hungarian) algorithm implementation by @addaleax

--- agents/assignment-solver.ts
+++ agents/assignment-solver.ts
@@ -3,6 +3,7 @@ import Settler from '../assignments/Settler'
 import { clearLog, log } from '../helpers/logging'
 import Turn from '../helpers/Turn'
 import { Agent } from '../lux/Agent'
+import hungarianMethod from '../helpers/hungarianMethod'
 
 const agent = new Agent()
 
@@ -36,12 +37,36 @@ async function main() {
     const assignments = getAssignments(turn)
     const units = turn.player.units
 
-    if (gameState.turn === 0) {
+    // if (gameState.turn === 0) {
+    //   for (const assignment of assignments) {
+    //     log(`${assignment.type} ${assignment.target.x} ${assignment.target.y}`)
+    //     for (const unit of units) {
+    //       log(`> cost(${unit.id}) = ${assignment.getCost(unit, turn)}`)
+    //     }
+    //   }
+    // }
+
+    const costMatrix: number[][] = []
+
+    for (const unit of units) {
+      const row = []
       for (const assignment of assignments) {
-        log(`${assignment.type} ${assignment.target.x} ${assignment.target.y}`)
-        for (const unit of units) {
-          log(`> cost(${unit.id}) = ${assignment.getCost(unit, turn)}`)
-        }
+        const cost = assignment.getCost(unit, turn)
+        row.push(cost)
+      }
+      costMatrix.push(row)
+    }
+
+    const start = new Date().getTime()
+    const solvedAssignments = hungarianMethod(costMatrix)
+    const time = new Date().getTime() - start
+
+    if (gameState.turn === 0) {
+      log(`${solvedAssignments.length} optimal assignments found in ${time}ms`)
+      for (const [unitIndex, assignmentIndex] of solvedAssignments) {
+        const unit = units[unitIndex]
+        const assignment = assignments[assignmentIndex]
+        log(`> ${unit.id} -> ${assignment.type} ${assignment.target.x} ${assignment.target.y} (cost=${costMatrix[unitIndex][assignmentIndex]})`)
       }
     }
 
