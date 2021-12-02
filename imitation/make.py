29acda5587d29af86bc17b207bd41e4cf4f96154
nbabcock <nathan.r.babcock@gmail.com>
2021-12-02 16:46:42 -0600

    Imitation learning bot from Kaggle notebook

---
+++ imitation/make.py
@@ -0,0 +1,9 @@
+from kaggle_environments import make
+import json
+
+env = make("lux_ai_2021", configuration={"width": 24, "height": 24, "loglevel": 2, "annotations": True, "out": "replays/python-replay.json"}, debug=True)
+steps = env.run(['imitation/agent.py', 'imitation/agent.py'])
+replay = env.toJSON()
+with open("replays/python.json", "w") as f:
+  json.dump(replay, f)
+#env.render(mode="ipython", width=1200, height=800)
